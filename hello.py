67f86d110f15297a981b727b6f1530360d0bfa4e
micah.igtanloc <micahkirsten.igtanloc@marcellin.school.nz>
2025-03-04 10:52:13 +1300

    Added more code

--- hello.py
+++ hello.py
@@ -4,5 +4,8 @@ print("My favorite colour is yellow")
 name = input("What is your name? ")
 print(f"Hi {name}!")
 
-age = input("How old are you? ")
+age = int(input("How old are you? "))
 print(f"{name} you're {age} years old?")
+
+if age > 25:
+    print("You are old")
