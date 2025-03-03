b484ce5c49dedd98b5745ec83857a79e0fc0b6b9
micah.igtanloc <micahkirsten.igtanloc@marcellin.school.nz>
2025-03-04 10:39:40 +1300

    Written more code

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 print("Hello, World!")
 print("My favorite colour is yellow")
+
+name = input("What is your name? ")
+print(f"Hi {name}!")
+
+age = input("How old are you? ")
+print(f"{name} you're {age} years old?")
