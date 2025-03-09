9c9116c2427a61b0ed5ec849ac1971b1bd162131
micah.igtanloc <micahkirsten.igtanloc@marcellin.school.nz>
2025-03-10 12:10:11 +1300

    code

---
+++ list.py
@@ -0,0 +1,14 @@
+colours = ["red", "green" , "yellow" , "blue" , "purple"]
+#print(colours[0])
+#print(colours[1])
+
+#for i in colours:
+#    print(i)
+
+#print(colours[-3])
+
+#print(len(colours)) 
+
+for i in colours:
+    if i == "yellow":
+        print("There is yellow") 
